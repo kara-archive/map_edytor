a3f34ac6e7daed368020a13d7792799c1d6f8818
kara-archive <72498164+kara-archive@users.noreply.github.com>
2024-12-31 17:22:46 +0100

    app

--- app.py
+++ app.py
@@ -2,12 +2,46 @@ import sys
 import argparse
 from PyQt5.QtWidgets import QApplication
 from views.main_view import MainView  # Zakładam, że to Twój główny widok
+import os
+import shutil
+from modes.add_point import AddPoints
+
+def ensure_resources_exist():
+    # Ścieżka do katalogu tymczasowego w PyInstaller
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+
+    # Ścieżki do folderów wbudowanych w aplikację
+    icons_path = os.path.join(base_path, "icons")
+    maps_path = os.path.join(base_path, "gotowe_mapki")
+    tury_path = os.path.join(base_path, "Tury")
+
+    # Ścieżki, gdzie mają być zapisane brakujące pliki
+    output_icons = os.path.join(os.getcwd(), "icons")
+    output_maps = os.path.join(os.getcwd(), "gotowe_mapki")
+    output_tury = os.path.join(os.getcwd(), "Tury")
+
+    # Tworzenie folderów, jeśli nie istnieją
+    if not os.path.exists(output_icons):
+        print("Tworzę brakujący folder 'icons'")
+        shutil.copytree(icons_path, output_icons)
+
+    if not os.path.exists(output_maps):
+        print("Tworzę brakujący folder 'gotowe_mapki'")
+        os.makedirs(output_maps, exist_ok=True)
+
+    if not os.path.exists(output_tury):
+        print("Tworzę brakujący folder 'Tury'")
+        os.makedirs(output_tury, exist_ok=True)
+
+# Wywołaj funkcję podczas uruchamiania programu
+ensure_resources_exist()
 
 def main():
     # Parsowanie argumentów
     parser = argparse.ArgumentParser(description="sram psa jak sra")
     parser.add_argument("--load", type=str, help="Ścieżka do pliku ZIP do wczytania przy uruchomieniu.")
     args = parser.parse_args()
+
 
     # Uruchomienie aplikacji
     app = QApplication(sys.argv)
@@ -20,6 +54,7 @@ def main():
             print(f"Pomyślnie wczytano plik: {args.load}")
         except Exception as e:
             print(f"Błąd podczas wczytywania pliku: {e}")
+
 
     # Pokaż główne okno
     main_view.show()
