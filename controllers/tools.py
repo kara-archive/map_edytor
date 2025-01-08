41d42d96ed401273955a28a386410fe968e3e3eb
kara-archive <72498164+kara-archive@users.noreply.github.com>
2025-01-08 06:35:57 +0100

    bez gówna

--- controllers/tools.py
+++ controllers/tools.py
@@ -2,8 +2,6 @@ from PyQt5.QtGui import QPainter, QColor, QPainter, QColor
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPainterPath, QPen, QColor
 from PyQt5.QtWidgets import QGraphicsPathItem
-import cv2 as cv
-import numpy as np
 
 def flood_fill(layer, x, y, color):
     # Pobranie wymiarów obrazu
